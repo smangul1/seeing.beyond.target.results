7a3e5af653fc27802bec3b326354a2900e603592
Serghei Mangul <smangul@ucla.edu>
2018-06-05 04:48:02 -0700

    add results

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -4,11 +4,11 @@ library(ggplot2)
 
 pdf("5S.pdf",width=10,height=4)
 library(ggplot2)
- ggplot(data, aes(x=disease, y= X5S.cov, fill=disease))+ geom_boxplot()
+ ggplot(data, aes(x=disease, y= X5S.cov/(intergenic.cov/1000000), fill=disease))+ geom_boxplot()
 dev.off()
 
 pdf("MT.pdf",width=10,height=4)
 library(ggplot2)
- ggplot(data, aes(x=disease, y= MT.cov, fill=disease))+ geom_boxplot()
+ ggplot(data, aes(x=disease, y= MT.cov/(intergenic.cov/1000000), fill=disease))+ geom_boxplot()
 dev.off()
 
